565053c9c467bfe4e2f1c4058c45a08270f1f0a3
haideralipunjabi <haiderali@cyberservices.com>
2016-09-26 19:07:28 +0530

    Uploaded base files

---
+++ AnagramBox/AnagramBox.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnagramBox
+{
+    public partial class AnagramBox: UserControl
+    {
+
+       static int numberBoxes = 4;
+        static Size size = new Size(20, 20);
+      static  RichTextBox[] textBoxes;
+       static string anagramText, shuffledText;
+        
+        
+        public AnagramBox()
+        {
+            InitializeComponent();
+            this.Size = new Size(size.Width * numberBoxes, size.Height);
+           
+            CreateBoxes();
+        }
+
+        public void CreateBoxes()
+        {
+            this.Controls.Clear();
+            textBoxes = new RichTextBox[numberBoxes];
+            size = new Size(this.Size.Width / numberBoxes, size.Height);
+            for (int i = 0; i < numberBoxes; i++)
+            {
+                textBoxes[i] = new RichTextBox();
+                textBoxes[i].Size = size;
+                textBoxes[i].Visible = true;
+                textBoxes[i].MaxLength = 1;
+                textBoxes[i].ReadOnly = false;
+                textBoxes[i].Font = this.Font;
+                textBoxes[i].Multiline = false;
+                textBoxes[i].Location = new Point(i * size.Width, 0);
+
+                this.Controls.Add(textBoxes[i]);
+            }
+        }
+
+        public void Shuffler()
+        {
+            shuffledText = anagramText.ToCharArray().Shuffle();
+            DistributeText(shuffledText); 
+        }
+
+        public void DistributeText(string text)
+        {
+            int characters = text.Length;
+            NumberOfBoxes = characters;
+            char[] characterArray = text.ToCharArray();
+            for(int i = 0; i < characters; i++)
+            {
+                textBoxes[i].Text = characterArray[i].ToString();
+            }
+        }
+
+       override public string Text
+        {
+            get
+            {
+                return anagramText;
+            }
+            set
+            {
+                anagramText = value;
+                DistributeText(anagramText);
+            }
+        }
+        public override Font Font
+        {
+            get
+            {
+                return base.Font;
+            }
+
+            set
+            {
+                base.Font = value;
+                CreateBoxes();
+            }
+        }
+
+
+
+
+        private void AnagramBox_Resize(object sender, EventArgs e)
+        {
+            size.Height = this.Size.Height;
+            size.Width = this.Size.Width / numberBoxes;
+            CreateBoxes();  
+        }
+
+        public int NumberOfBoxes
+        {
+            get
+            {
+                return numberBoxes;
+            }
+            set
+            {
+                numberBoxes = value;
+                CreateBoxes();
+            }
+        }
+    }
+
+   public static class CharArrayExten
+    {
+        public static string Shuffle(this char[] array)
+        {
+            var random = new Random();
+            for (int x = 0; x < 100; x++)
+            {
+                for (int i = array.Length; i > 1; i--)
+                {
+                    // Pick random element to swap.
+                    int j = random.Next(i); // 0 <= j <= i-1
+                                            // Swap.
+                    char tmp = array[j];
+                    array[j] = array[i - 1];
+                    array[i - 1] = tmp;
+                }
+            }
+            string s = new string(array);
+            return s;
+        }
+
+    }
+}
---
+++ AnagramBoxesTest/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AnagramBoxesTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.anagramBox1 = new AnagramBox.AnagramBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Shuffle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(87, 157);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // anagramBox1
+            // 
+            this.anagramBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.anagramBox1.Location = new System.Drawing.Point(23, 12);
+            this.anagramBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.anagramBox1.Name = "anagramBox1";
+            this.anagramBox1.NumberOfBoxes = 6;
+            this.anagramBox1.Size = new System.Drawing.Size(234, 50);
+            this.anagramBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.anagramBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private AnagramBox.AnagramBox anagramBox1;
+    }
+}
+
